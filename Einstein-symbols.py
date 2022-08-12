339efe5dd6cba02f88aa24995af88b29ef9d6325
Reza Rahemi <110694001+quantizedreza@users.noreply.github.com>
2022-08-12 14:36:22 -0400

    Delete Einstein-symbols.py

--- Einstein-symbols.py
+++
@@ -1,9 +0,0 @@
-import sympy
-from einsteinpy.symbolic import ChristoffelSymbols, RiemannCurvatureTensor
-from einsteinpy.symbolic.predefined import Schwarzschild
-
-sympy.init_printing()
-
-
-sw = Schwarzschild()
-sw.tensor()
